3b094dcaf6ffcfd363cbb4c29661ef6c3efa3c46
kiv10 <55044507+kiv10@users.noreply.github.com>
2020-10-27 15:49:39 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = 'thekey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-  
-   
-   
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
